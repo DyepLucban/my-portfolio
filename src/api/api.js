185371ba91921a9ea5e7249c366650c40f4893f4
Jeffrey Lucban <lucbanjep@gmail.com>
2020-07-02 12:56:28 +0800

    heroku commit

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,11 @@ import axios from 'axios'
 export default {
 
     async sendNewEmail(params) {
-        return await axios.post( endpoints.email.send_email, params)
+        try {
+            return await axios.post( endpoints.email.send_email, params)
+        } catch (error) {
+            return error.response.data
+        }
     },
 
 }
--- src/components/Contact/ContactFormComponent.vue
+++ src/components/Contact/ContactFormComponent.vue
@@ -3,26 +3,30 @@
         <b-form>
             <b-row>
                 <b-col lg="6">
-                    <b-form-group label="Email address:" label-for="email" description="I'll never share your email with anyone else.">
-                        <b-form-input v-model="data.email" autocomplete="off" size="lg" type="email" placeholder="Enter email"></b-form-input>
+                    <b-form-group label="Email address:" description="I'll never share your email with anyone else.">
+                        <b-form-input v-model="data.email" autocomplete="off" size="lg" type="email" placeholder="Enter email" :state="this.error.email.state"></b-form-input>
+                        <b-form-invalid-feedback id="email-feedback">{{ this.error.email.message }}</b-form-invalid-feedback>                        
                     </b-form-group>
                 </b-col>
 
                 <b-col lg="6">
-                    <b-form-group label="Your Name:" label-for="input-2">
-                        <b-form-input v-model="data.name" autocomplete="off" size="lg" placeholder="Enter name"></b-form-input>
+                    <b-form-group label="Your Name:">
+                        <b-form-input v-model="data.name" autocomplete="off" size="lg" placeholder="Enter name" :state="this.error.name.state"></b-form-input>
+                        <b-form-invalid-feedback id="name-feedback">{{ this.error.name.message }}</b-form-invalid-feedback>                        
                     </b-form-group>
                 </b-col>
 
                 <b-col lg="12">
-                    <b-form-group label="Subject" label-for="input-2">
-                        <b-form-input v-model="data.subject" autocomplete="off" size="lg" placeholder="Enter Subject"></b-form-input>
+                    <b-form-group label="Subject">
+                        <b-form-input v-model="data.subject" autocomplete="off" size="lg" placeholder="Enter Subject" :state="this.error.subject.state"></b-form-input>
+                        <b-form-invalid-feedback id="subject-feedback">{{ this.error.subject.message }}</b-form-invalid-feedback>                        
                     </b-form-group>
                 </b-col>
 
                 <b-col>
-                    <b-form-group label="Your Message:" label-for="input-3">
-                        <b-textarea v-model="data.message" size="lg"></b-textarea>
+                    <b-form-group label="Your Message:">
+                        <b-textarea v-model="data.message" size="lg" :state="this.error.message.state"></b-textarea>
+                        <b-form-invalid-feedback id="message-feedback">{{ this.error.message.message }}</b-form-invalid-feedback>                        
                     </b-form-group>
                     <b-button size="lg" variant="info" @click="sendEmail">Submit</b-button>   
                 </b-col>
@@ -47,7 +51,26 @@ export default {
             name: '',
             subject: '',
             message: '',
-        }
+        },
+        error: {
+            email: {
+                state: null,
+                message: '',
+            },
+            name: {
+                state: null,
+                message: '',
+            },
+            subject: {
+                state: null,
+                message: '',
+            },
+            message: {
+                state: null,
+                message: '',
+            },                                    
+        },
+        state: null,
     }),
 
 
@@ -56,9 +79,30 @@ export default {
 
         async sendEmail() {
             let res = await this.sendNewEmail(this.data);
-            if(res) {
+
+            if(res.status == 200) {
                 this.data = ''
-                this.$swal('Your Email is Successfully Sent!');
+                this.error.email.state = null;
+                this.error.name.state = null;
+                this.error.subject.state = null;
+                this.error.message.state = null;
+                this.$swal(res.data.message);
+            } else if (res.code == 422) {
+                this.error.email.message = (res.message.email) ? res.message.email.toString() : ''
+                this.error.email.state = (res.message.email) ? false : true
+                this.error.name.message = (res.message.name) ? res.message.name.toString() : ''
+                this.error.name.state = (res.message.name) ? false : true
+                this.error.subject.message = (res.message.subject) ? res.message.subject.toString() : ''
+                this.error.subject.state = (res.message.subject) ? false : true
+                this.error.message.message = (res.message.message) ? res.message.message.toString() : ''
+                this.error.message.state = (res.message.message) ? false : true
+            } else {
+                this.$swal(res.message);
+                this.data = ''
+                this.error.email.state = null;
+                this.error.name.state = null;
+                this.error.subject.state = null;
+                this.error.message.state = null;                
             }
         }
     }
