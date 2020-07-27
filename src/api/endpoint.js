a1faf41ec1ae9a72e990da571f1ce337d011f133
Jeffrey Lucban <lucbanjep@gmail.com>
2020-07-27 19:55:20 +0800

    update endpoint

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,6 +1,6 @@
 export default {
 	email: {
-        send_email: 'https://heroku-blog-api.herokuapp.com/api/send-email',
+        send_email: 'https://lucban-e-commerce-api.herokuapp.com/api/send-email',
         // send_email: 'http://127.0.0.1:8000/api/send-email',
     },
 }
