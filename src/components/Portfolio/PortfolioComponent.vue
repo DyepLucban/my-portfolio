f805ca94a9b7bc06262995af381c667e0bb1397e
Jeffrey Lucban <lucbanjep@gmail.com>
2020-07-28 13:43:22 +0800

    update portfolio module

--- src/components/Portfolio/PortfolioComponent.vue
+++ src/components/Portfolio/PortfolioComponent.vue
@@ -8,15 +8,19 @@
         </b-row>
         <b-row>
             <b-col class="mb-4" lg="4" v-for="(item, k) in works" :key="k">
-                <b-card :title="item.title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 25rem;" class="mb-2">
+                <b-card :title="item.title" :img-src="item.image" img-alt="Image" img-top tag="article" style="max-width: 25rem;" class="mb-2">
                     <b-card-text>
                         {{ item.description }}
                     </b-card-text>
-                    <b-card-text>
-                        {{ item.language_used }}
-                    </b-card-text>
 
-                    <b-button variant="primary">View Project</b-button>
+                    <h6>Tech Used:</h6>
+                    <b-card-text>
+                        <div v-for="(lang, k) in item.language_used" :key="k" style="float:left;">
+                            <b-badge pill variant="success" class="mr-1">{{ lang.name }}</b-badge>
+                        </div>
+                    </b-card-text>
+                    <br><br>
+                    <a class="btn btn-primary" variant="primary" :href="item.url" target="_blank">View Project</a>
                 </b-card>                
             </b-col>
         </b-row>
@@ -33,24 +37,44 @@ export default {
         return {
             works: [
                 {
-                    title: 'Project 1',
-                    description: 'Description 1',
-                    language_used: 'language',
+                    title: 'Library System',
+                    description: 'This system allows the user to borrow books.',
+                    url: 'https://lucban-library-system.herokuapp.com/',
+                    image: require('@/assets/images/library_system.png'),
+                    language_used: [
+                        { name: 'Laravel' },
+                        { name: 'Vue.js' },
+                        { name: 'MySQL' },
+                    ],
                 },
                 {
                     title: 'Project 2',
-                    description: 'Description 2',
-                    language_used: 'language',
+                    description: 'Still under recoding',
+                    url: '#',
+                    image: require('@/assets/images/under_construction.png'),                    
+                    language_used: [
+                        {},
+                    ],
                 },
                 {
                     title: 'Project 3',
-                    description: 'Description 3',
-                    language_used: 'language',
+                    description: 'Still under recoding',
+                    url: '#',
+                    image: require('@/assets/images/under_construction.png'),                    
+                    language_used: [
+                        {},
+                    ],
+
                 },
                 {
                     title: 'Project 4',
-                    description: 'Description 4',
-                    language_used: 'language',
+                    description: 'Still under recoding',
+                    url: '#',
+                    image: require('@/assets/images/under_construction.png'),                    
+                    language_used: [
+                        {},
+                    ],
+
                 },                                                                               
             ],
         }
